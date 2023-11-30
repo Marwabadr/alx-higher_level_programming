6e775df003e2f9a1c918a41ea7574f0bd3b375ea
Marwabadr <elbadrmarwa2002@gmail.com>
2023-11-30 09:08:04 -0800

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return sub(c, 5)
+        return (c)
 
     else:
         return (sub(a, b))
